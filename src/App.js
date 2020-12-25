c4cc7b2b1918120a3d0cd9326031d05e5246b198
Cian O'Boyle <cianob99@live.com>
2020-12-25 00:12:53 +0000

    updated css

--- src/App.js
+++ src/App.js
@@ -26,30 +26,40 @@ function App() {
         {/* <img src={countryInfo.countryInfo.flag}/> */}
         
       </div>
-      <div className="totalStats">
-        <h3>Total Stats</h3>
-        <ul> 
-          <li>Total Cases: {numeral(countryInfo.cases).format("0,0")}</li>
-          <li>Total Deaths: {numeral(countryInfo.totalDeaths).format("0,0")}</li>
-          <li>Total Recovered: {numeral(countryInfo.recovered).format("0,0")}</li>
-        </ul>
-      </div>
-      <div className="dailyStats">
-        <h3>Todays Stats</h3>
-        <ul>
-          <li>Todays Cases: {numeral(countryInfo.todayCases).format("0,0")}</li>
-          <li>Todays Deaths: {numeral(countryInfo.todayDeaths).format("0,0")}</li>
-          <li>Todays Recovered: {numeral(countryInfo.todayRecovered).format("0,0")}</li>
-        </ul>
-      </div>
-      <div className="activeStats">
-        <ul>
-          <li>Population: {numeral(countryInfo.population).format("0,0")}</li>
-          <li>Active Cases: <strong>{numeral(countryInfo.active).format("0,0")}</strong></li>
-          <li>Of which are critical: <strong>{numeral(countryInfo.critical).format("0,0")}</strong></li>
-        </ul>
-      </div>
-    
+      <table className="stats">
+        <tr>
+          <td><h3>Total Stats</h3></td>
+          <td><h3>Todays Stats</h3></td>
+          <td><h3>Overall Stats</h3></td>
+        </tr>
+
+        <tr>
+          <td className="totalStats">
+            <ul>
+              <li>Total Cases: {numeral(countryInfo.cases).format("0,0")}</li>
+              <li>Total Deaths: {numeral(countryInfo.totalDeaths).format("0,0")}</li>
+              <li>Total Recovered: {numeral(countryInfo.recovered).format("0,0")}</li>
+            </ul>
+          </td>
+
+          <td className="dailyStats">
+            <ul>
+              <li>Todays Cases: {numeral(countryInfo.todayCases).format("0,0")}</li>
+              <li>Todays Deaths: {numeral(countryInfo.todayDeaths).format("0,0")}</li>
+              <li>Todays Recovered: {numeral(countryInfo.todayRecovered).format("0,0")}</li>
+            </ul>
+          </td>
+
+          <td className="overallStats">
+            <ul>
+              <li>Population: {numeral(countryInfo.population).format("0,0")}</li>
+              <li>Total Tests: {numeral(countryInfo.tests).format("0,0")}</li>
+              <li>Active Cases: <strong>{numeral(countryInfo.active).format("0,0")}</strong></li>
+              <li>Of which are critical: <strong>{numeral(countryInfo.critical).format("0,0")}</strong></li>
+            </ul>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
